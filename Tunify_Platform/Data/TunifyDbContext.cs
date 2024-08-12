ac28634c25112352757ba5a73b5cfd7b4212bd29
HANAN ALHADE <23041786@student.ltuc.com>
2024-08-12 13:09:12 +0300

    Add project files.

---
+++ Tunify_Platform/Data/TunifyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Tunify_Platform.Data
+{
+    public class TunifyDbContext :DbContext
+    {
+        public TunifyDbContext (DbContextOptions options) :base(options)
+        {
+
+        }
+
+    }
+}
---
+++ Tunify_Platform/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Tunify_Platform.Models
+{
+    public class User
+    {
+        public int UserId { get; set; } 
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Join_Date { get; set; }
+        public int Subscription_ID { get; set; } 
+    }
+}
